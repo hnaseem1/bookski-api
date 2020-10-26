3762cf38936fe22c4666d2eae37b17daec1de372
Hasan Naseem <hn92@live.com>
2020-10-26 15:55:40 -0400

    user create and show routes working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    
+    def show
+        render :json => User.all
+    end
+
+    def create
+        @user = User.create!(user_params)
+        render :json => @user
+    end
+
+    private 
+    
+    def user_params
+        # whitelist params
+        params.permit(:username)
+    end
 end
