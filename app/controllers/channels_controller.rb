822cdc5571591825e6c49851512b18074fe2f328
Hasan Naseem <hn92@live.com>
2020-10-26 18:04:25 -0400

    rest routes manually working

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,16 @@
 class ChannelsController < ApplicationController
+    def create
+        @channel = Channel.create!(channel_params)
+        render :json => @channel
+    end
+
+    def index 
+        render :json => Channel.all
+    end
+
+    private
+
+    def channel_params
+        params.permit(:name)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show]
     
     def show
+        render :json => @user
+    end
+
+    def index
         render :json => User.all
     end
 
@@ -9,10 +14,19 @@ class UsersController < ApplicationController
         render :json => @user
     end
 
+    # a route to login as a user using username
+    # a route to send a message to a another user
+    # a route to send a message on a channel
+
     private 
     
     def user_params
         # whitelist params
         params.permit(:username)
+    end
+
+    def set_user
+        # whitelist params
+        @user = User.where(username: params[:id])
     end
 end
